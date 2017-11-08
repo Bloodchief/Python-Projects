8c49bc1516fec187ee715f66a4971baf0ea694e3
Bloodchief <33470456+Bloodchief@users.noreply.github.com>
2017-11-07 19:31:25 -0500

    Add files via upload

---
+++ WebParser.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import requests
+from bs4 import BeautifulSoup
+
+
+#Defines link to grab data from
+link = "http://www.foxnews.com/politics/2017/11/06/democrat-walks-out-moment-silence-for-texas-massacre-victims.html"
+r = requests.get(link)
+
+#Assigns page to 'data'
+data = r.text
+
+#soups the entire webpage
+soup = (BeautifulSoup(data))
+
+#sets <p> wrapped data to variable paragraphs
+paragraphs = (soup.find_all('p'))
+paragraphs.prettify()
+print(paragraphs)
+#Writes paragraphs to file
+#file=open("NewFile.txt", "w")
+#file.write(paragraphs)
+#file.close()
+
+
+#keeps window open
+input("Press enter to quit.")
